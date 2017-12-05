d7d3b0c59f70363ff37e2f4ee6f4631fab829bad
Mark Magro <mcrmagro@gmail.com>
2017-12-05 22:32:54 +0100

    Added sources.

---
+++ Assets/Scripts/TextWindowScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWindowScript : MonoBehaviour
+{
+    public RectTransform content;
+    Text textComponent;
+
+    void Start()
+    {
+        textComponent = content.transform.Find("Text").GetComponent<Text>();
+        textComponent.text = "";
+
+        DisplaySystemInfo();
+    }
+
+    void Update()
+    {    
+	}
+
+    void Append(string str)
+    {
+        textComponent.text += str;
+    }
+
+    void DisplaySystemInfo()
+    {
+        Display(" Device model        : ", SystemInfo.deviceModel);
+        Display(" Device name         : ", SystemInfo.deviceName);
+        Display(" Device type         : ", SystemInfo.deviceType.ToString());
+        Display(" Device name         : ", SystemInfo.graphicsDeviceName);
+        Display(" Device type         : ", SystemInfo.graphicsDeviceType.ToString());
+        Display(" Device vendor       : ", SystemInfo.graphicsDeviceVendor);
+        Display(" Device version      : ", SystemInfo.graphicsDeviceVersion);
+        Display(" Shader level        : ", SystemInfo.graphicsShaderLevel);
+        Display(" UV starts at top    : ", SystemInfo.graphicsUVStartsAtTop);
+        Display(" Max texture size    : ", SystemInfo.maxTextureSize);
+        Display(" OS                  : ", SystemInfo.operatingSystem);
+        Display(" OS family           : ", SystemInfo.operatingSystemFamily.ToString());
+        Display(" Processor count     : ", SystemInfo.processorCount);
+        Display(" Processor Type      : ", SystemInfo.processorType);
+        Display(" Render target count : ", SystemInfo.supportedRenderTargetCount);
+        Display(" 2D array textures   : ", SystemInfo.supports2DArrayTextures);
+        Display(" 3D render textures  : ", SystemInfo.supports3DRenderTextures);
+        Display(" 3D textures         : ", SystemInfo.supports3DTextures);
+        Display(" Compute shaders     : ", SystemInfo.supportsComputeShaders);
+    }
+
+    void Display(string str, bool val)
+    {
+        Append(str + (val ? "Yes" : "No") + "\n");
+    }
+
+    void Display(string str, string val)
+    {
+        Append(str + val + "\n");
+    }
+
+    void Display(string str, int val)
+    {
+        Append(str + val + "\n");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BasicTest
+
+This is a basic Unity test. It creates a scroll view containing text and is populated with some system information.
